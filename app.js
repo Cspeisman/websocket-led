81c605581c61ca74a47e255a110751c980d4b4ec
Cspeisman <Cspeisman@gmail.com>
2014-10-02 19:48:42 -0400

    testing

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 var server = require('http').Server(app)
-var io = require('socket.io')(server);
+// var io = require('socket.io')(server);
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
 server.listen(3000);
 
@@ -12,10 +12,10 @@ app.get('/', function(req, res){
   res.render('index')
 })
 
-io.on('connection', function (socket) {
-  console.log("WOOOOOOOOOO")
-  socket.on('stateChanged', function (state) {
-    console.log("State changed: " + state);
-    io.emit("updateState", state);
-  });
-});
+// io.on('connection', function (socket) {
+//   console.log("WOOOOOOOOOO")
+//   socket.on('stateChanged', function (state) {
+//     console.log("State changed: " + state);
+//     io.emit("updateState", state);
+//   });
+// });
