79a6afe30ef533282ecec029f47dbbfaad1b0d09
Cspeisman <Cspeisman@gmail.com>
2014-10-02 22:03:19 -0400

    big bucks no whammys

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var express = require('express')
+var express = require('express');
 var app = express();
-var server = require('http').Server(app)
+app.use(express.static(__dirname + '/public'));
+var server = require('http').createServer(app).listen(process.env.PORT || 8080);
+var io = require('socket.io')(server);
 
-server.listen(5000)
-app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('index.ejs')
+app.get('/', function(req, res){
+  res.render('index')
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+io.on('connection', function (socket) {
+  console.log("WOOOOOOOOOO")
+  socket.on('stateChanged', function (state) {
+    console.log("State changed: " + state);
+    io.emit("updateState", state);
+  });
+});
